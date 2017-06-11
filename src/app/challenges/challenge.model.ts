dd65544120a3b05909d37293c20c4e67bfc24717
ryanj89 <ryanj89@gmail.com>
2017-06-11 14:43:20 -0600

    Updated challenge model

--- src/app/challenges/challenge.model.ts
+++ src/app/challenges/challenge.model.ts
@@ -7,17 +7,15 @@ export class Challenge {
   public creator_id: number;
   public video_url: string;
   public category: string;
-  public points: number;
   public expires_at: Date;
   public is_private: boolean;
   
-  constructor(name: string, desc: string, creator_id: number, video_url: string, category: string, points: number, expires_at: Date, is_private: boolean) {
+  constructor(name: string, desc: string, creator_id: number, video_url: string, category: string, expires_at: Date, is_private: boolean) {
     this.name = name;
     this.description = desc;
     this.creator_id = creator_id;
     this.video_url = video_url;
     this.category = category;
-    this.points = points;
     this.expires_at = expires_at;
     this.is_private = is_private;
   }
